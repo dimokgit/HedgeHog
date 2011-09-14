776ebfbf24d575bc31d6bbc52acbda05367fc510
dimokhot@hotmail.com <dimokhot@hotmail.com@84aa1c28-c8c8-1687-32ec-430e942c4232>
2011-09-14 11:25:49 +0000

    Kamikaze Zero

--- trunk/HedgeHog.Extentions/Collection.cs
+++ trunk/HedgeHog.Extentions/Collection.cs
@@ -7,7 +7,7 @@ using System.Collections.ObjectModel;
 namespace HedgeHog {
   public static class CollectionEx {
     public static void RemoveAll<T>(this IList<T> c,IEnumerable<T> elements ){
-      foreach (var e in elements)
+      foreach (var e in elements.ToList())
         c.Remove(e);
     }
   }
