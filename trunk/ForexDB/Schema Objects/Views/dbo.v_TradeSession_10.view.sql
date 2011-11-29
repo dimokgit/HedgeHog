d087f29738aad768333d70d17ae183626c0d2486
dimokhot@hotmail.com <dimokhot@hotmail.com@84aa1c28-c8c8-1687-32ec-430e942c4232>
2011-11-29 00:23:31 +0000

    Forex DB

---
+++ trunk/ForexDB/Schema Objects/Views/dbo.v_TradeSession_10.view.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW dbo.v_TradeSession_10
+AS
+SELECT     TOP (100) PERCENT Pair, SessionId, MAX(TimeStamp) AS TimeStamp, COUNT(*) AS Count, SUM(GrossPL) AS GrossPL, DATEDIFF(dd, MIN(TimeOpen), 
+                      MAX(TimeClose)) AS Days, MAX(Lot) AS Lot, AVG(Lot) AS LotA, STDEV(Lot) AS LotSD, SUM(GrossPL) / NULLIF (DATEDIFF(dd, MIN(TimeOpen), MAX(TimeClose)), 0) 
+                      * 30.0 AS DollarsPerMonth, CONVERT(numeric(10, 2), AVG(PL)) AS PL, DATEDIFF(n, MIN(TimeStamp), MAX(TimeStamp)) AS MinutesInTest, CONVERT(float, 
+                      DATEDIFF(dd, MIN(TimeOpen), MAX(TimeClose))) / NULLIF (DATEDIFF(n, MIN(TimeStamp), MAX(TimeStamp)), 0.0) AS DaysPerMinute, SessionInfo
+FROM         dbo.t_Trade
+WHERE     (IsVirtual = 1)
+GROUP BY SessionId, Pair, SessionInfo
+HAVING      (COUNT(*) >= 10)
+ORDER BY TimeStamp DESC
