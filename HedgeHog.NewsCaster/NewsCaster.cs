1c796cfba1f0164118e88081d189bb844e3e096a
dimokhot@hotmail.com <dimokhot@hotmail.com@84aa1c28-c8c8-1687-32ec-430e942c4232>
2013-05-26 03:39:55 +0000

    NewsCaster: Blommberg 0.1.0

--- HedgeHog.NewsCaster/NewsCaster.cs
+++ HedgeHog.NewsCaster/NewsCaster.cs
@@ -13,8 +13,35 @@ namespace HedgeHog.NewsCaster {
     public DateTimeOffset Time { get; set; }
   }
   public static class NewsHound {
-    public static IEnumerable<NewsEvent> Fetch() {
-      throw new NotImplementedException();
+    public static class Bloomberg {
+      private static IEnumerable<IEnumerable<NewsEvent>> Parse(HAP.HtmlDocument doc) {
+        var tableTabSpace = doc.DocumentNode.SelectSingleNode("//body//table//tr/td[@class='tabspace']");
+        var tableCaNavMon = tableTabSpace.SelectSingleNode("//table[@class='calnavmon']");
+        var tableEvents = tableTabSpace.SelectSingleNode("//table[@class='eventstable']");
+        var tds = tableEvents.SelectNodes("tr").Skip(1).First().SelectNodes("td");
+        var news = tds.Aggregate(new List<IEnumerable<NewsEvent>>(),
+        (list0, td) => {
+          var ne = td.SelectNodes("div[@class='econoevents']")
+            .Aggregate(new List<NewsEvent>(),
+              (list1, div) => {
+                var n = new NewsEvent() {
+                  Name = div.ChildNodes[0].InnerText,
+                  Time = DateTime.Parse(div.ChildNodes[1].InnerText.Replace("&#160", "").Split(';')[0])
+                };
+                list1.Add(n);
+                return list1; });
+          list0.Add(ne)  ;
+          return list0;
+        });
+        return news;
+      }
+      public static IEnumerable<IEnumerable<NewsEvent>> Fetch() {
+        return NewsHound.Fetch("http://bloomberg.econoday.com/byweek.asp",Parse);
+      }
+    }
+    static IEnumerable<IEnumerable<NewsEvent>> Fetch(string url, Func<HAP.HtmlDocument, IEnumerable<IEnumerable<NewsEvent>>> parser) {
+      var doc = new HAP.HtmlWeb().Load(url);
+      return parser(doc);
     }
   }
 }
