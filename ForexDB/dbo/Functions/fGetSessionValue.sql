c08e1ee3075753c0823b51f08591ce00736e5c20
dimokhot@hotmail.com <dimokhot@hotmail.com@84aa1c28-c8c8-1687-32ec-430e942c4232>
2013-06-16 15:35:35 +0000

    Added clrSplit

--- ForexDB/dbo/Functions/fGetSessionValue.sql
+++ ForexDB/dbo/Functions/fGetSessionValue.sql
@@ -6,6 +6,6 @@ RETURN(
 SELECT REPLACE(PARSENAME(Split,1),';','.')Value
 FROM
 (
-SELECT REPLACE(REPLACE(S1.Split,'.',';'),':','.') Split FROM Split(@Text,',') S1
+SELECT REPLACE(REPLACE(S1.Value,'.',';'),':','.') Split FROM clrSplit(@Text,',') S1
 )T WHERE PARSENAME(Split,2) = @Name
 )
