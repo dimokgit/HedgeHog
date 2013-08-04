ec5b88c95220cdf333e9127a80da8a3dd400ee18
dimokhot@hotmail.com <dimokhot@hotmail.com@84aa1c28-c8c8-1687-32ec-430e942c4232>
2013-08-04 03:42:14 +0000

    Corridor by FFT wieghted height.

--- trunk/HedgeHog.Alice.Store/TradingMacro/TradingMacro.ScanCorridor.Lib.cs
+++ trunk/HedgeHog.Alice.Store/TradingMacro/TradingMacro.ScanCorridor.Lib.cs
@@ -5,9 +5,21 @@ using System.Text;
 using System.Reactive.Linq;
 using System.Reactive.Concurrency;
 using HedgeHog.Bars;
+using System.Collections.Concurrent;
 
 namespace HedgeHog.Alice.Store {
   partial class TradingMacro {
+    private IList<int> MACrosses(IList<Rate> rates, int frame) {
+      var rates1 = rates.Zip(rates.Skip(1), (f, s) => new { f= f.PriceAvg, s=s.PriceAvg, ma = f.PriceCMALast }).ToArray();
+      var crosses = new int[0].ToConcurrentQueue();
+      Partitioner.Create(Enumerable.Range(0, rates.Count - frame).ToArray(), true).AsParallel()
+        .ForAll(i => {
+          var rates2 = rates1.ToArray(frame);
+          Array.Copy(rates1, i, rates2, 0, frame);
+          crosses.Enqueue(rates2.Count(r => r.f.Sign(r.ma) != r.s.Sign(r.ma)));
+        });
+      return crosses.ToArray();
+    }
     private CorridorStatistics ScanCorridorByHorizontalLineCrosses1(IList<Rate> ratesForCorridor, Func<Rate, double> priceHigh, Func<Rate, double> priceLow) {
       double level;
       var rates = CorridorByVerticalLineCrosses2(ratesForCorridor.ReverseIfNot(), _priceAvg, CorridorDistanceRatio.ToInt(), out level);
