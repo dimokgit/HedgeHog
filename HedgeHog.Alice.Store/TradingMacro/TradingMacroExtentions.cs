846a8e0d34ae08fcda4adef52e987b3b2a92f464
dimokdimon@gmail.com <dimokdimon@gmail.com@84aa1c28-c8c8-1687-32ec-430e942c4232>
2014-05-14 22:57:30 +0000

    Spike30

--- HedgeHog.Alice.Store/TradingMacro/TradingMacroExtentions.cs
+++ HedgeHog.Alice.Store/TradingMacro/TradingMacroExtentions.cs
@@ -2770,6 +2770,7 @@ namespace HedgeHog.Alice.Store {
         case ScanCorridorFunction.Spike: return ScanCorridorBySpike;
         case ScanCorridorFunction.Spike2: return ScanCorridorBySpike23;
         case ScanCorridorFunction.Spike24: return ScanCorridorBySpike24;
+        case ScanCorridorFunction.Spike30: return ScanCorridorBySpike30;
       }
       throw new NotSupportedException(function + "");
     }
