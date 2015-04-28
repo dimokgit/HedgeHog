ad7f7f66523662a83c5046557db7edc82edd7226
dimokgit <dimokhot@hotmail.com>
2015-04-27 22:18:51 -0400

    HubPipline,favicon

---
+++ HedgeHog.Alice.Client/Classes/MyHubPipelineModul.cs
@@ -0,0 +1,18 @@
+﻿using HedgeHog.Shared;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HedgeHog.Alice.Client {
+  public class MyHubPipelineModule : HubPipelineModule {
+    protected override void OnIncomingError(ExceptionContext exceptionContext, IHubIncomingInvokerContext invokerContext) {
+      dynamic caller = invokerContext.Hub.Clients.Caller;
+      caller.ExceptionHandler(exceptionContext.Error.Message);
+      GalaSoft.MvvmLight.Messaging.Messenger.Default.Send<LogMessage>(new LogMessage(exceptionContext.Error));
+      //exceptionContext.Result = new { error = exceptionContext.Error.Message };
+    }
+  }
+}
