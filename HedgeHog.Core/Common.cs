3ee13ec37b1bffbd2034fc7cc57243e1b53bd68c
Dmitri Lapchine <dimokhot@hotmail.com>
2017-09-25 08:41:36 -0400

    Release fix

--- HedgeHog.Core/Common.cs
+++ HedgeHog.Core/Common.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Converters;
 
 namespace HedgeHog {
   public static class Common {
