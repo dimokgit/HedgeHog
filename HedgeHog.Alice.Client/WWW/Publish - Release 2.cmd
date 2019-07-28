5bd1750e5e2e45011a3a7a120d213348cccde677
dimokgit <dimokdimon@gmail.com>
2019-07-28 11:21:37 -0400

    SignalR - update

--- HedgeHog.Alice.Client/WWW/Publish - Release 2.cmd
+++ HedgeHog.Alice.Client/WWW/Publish - Release 2.cmd
@@ -3,5 +3,6 @@ copy "trader4.html" "E:\Data\Forex\Builds\Release 2\WWW\Trader4.html"
 copy "d3-knockout\custom-binding-handlers\trend-line-chart-binding.js" "E:\Data\Forex\Builds\Release 2\WWW\d3-knockout\custom-binding-handlers\trend-line-chart-binding.js"
 REM xcopy "styles" "E:\Data\Forex\Builds\Release 2\WWW\styles" /s /y /i /D
 xcopy "Scripts" "E:\Data\Forex\Builds\Release 2\WWW\scripts" /s /y /i /D
-xcopy "bower_components" "E:\Data\Forex\Builds\Release 2 \WWW\bower_components" /s /y /i /D
+xcopy "bower_components" "E:\Data\Forex\Builds\Release 2\WWW\bower_components" /s /y /i /D
+xcopy "node_modules" "E:\Data\Forex\Builds\Release 2\WWW\node_modules" /s /y /i /D
 pause
