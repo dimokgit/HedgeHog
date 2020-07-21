2aadab10f32bdd9116786d907509e6841962ca29
dimokgit <dimokdimon@gmail.com>
2020-07-21 14:33:28 -0400

    Contract - Key based on Leg.Contract.LocalSymbol

--- TWS/IBApi.CSharpAPI/ComboLeg_p.cs
+++ TWS/IBApi.CSharpAPI/ComboLeg_p.cs
@@ -8,5 +8,15 @@ namespace IBApi {
   partial class ComboLeg {
     public bool IsBuy => Action.ToUpper() == "BUY";
     public int Quantity => IsBuy ? Ratio : -Ratio;
+
+    public override bool Equals(object obj) => obj is ComboLeg leg && ConId == leg.ConId && Ratio == leg.Ratio && Action == leg.Action;
+
+    public override int GetHashCode() {
+      var hashCode = 228620681;
+      hashCode = hashCode * -1521134295 + ConId.GetHashCode();
+      hashCode = hashCode * -1521134295 + Ratio.GetHashCode();
+      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Action);
+      return hashCode;
+    }
   }
 }
